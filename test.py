62662f85cdb89dd974c21249546cd167811ee763
Šárka Lišková <sarkaliskova@Sarka-MacBook-Air.local>
2020-08-19 18:55:11 +0200

    Back and forth

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
-from selenium import webdriver
 from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://digitalninomadstvi.cz")
 
-sleep(5)
+sleep(3)
+HomeURL = "https://digitalninomadstvi.cz"
+driver.get(HomeURL)
 
+sleep(1)
+
+elementCestovani = driver.find_element_by_xpath('//*[@id="menu-item-6330"]/a')
+# if(element.is_displayed())
+elementCestovani.click()
+
+sleep(1)
+driver.back()
+sleep(2)
+
+elementPodcast = driver.find_element_by_xpath('//*[@id="menu-item-9441"]/a')
+elementPodcast.click()
+
+sleep(1)
+driver.back()
+sleep(1)
 driver.close()
